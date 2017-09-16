ad34650162451e08d0024a5ece86a8a416bd6a1a
VictorMateusFerreira <victormateus.lucasferreira@gmail.com>
2017-09-16 18:48:44 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+
+//alert("Fui")
+//var aluno = {
+//	nome: "Fulaninho",
+//	telefone: "48 99554 6666",
+//	idade: 37
+//}
+function mudarCor(){ 
+	var titulo = document.querySelector("h1");
+	titulo.style.color = "purple";
+}
